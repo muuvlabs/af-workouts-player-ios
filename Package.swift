f0fab95dc3adec9f94d39ffd6e0049792f450b4e
Cristian Barril <cristian.barril@muuvlabs.com>
2023-01-10 17:53:20 -0300

    WorkoutsPlayerCore dependency removed from Package

--- Package.swift
+++ Package.swift
@@ -15,29 +15,14 @@ let package = Package(
     products: [
         .library(
             name: "WorkoutsPlayer",
-            targets: ["Dependencies", "WorkoutsPlayer"]
+            targets: ["WorkoutsPlayer"]
         )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/muuvlabs/workouts-player-core-internal-ios.git",
-            branch: "main"
-        )
+        // .package(url: /* package url */, from: "1.0.0"),        
     ],
     targets: [
-        .target(
-            name: "Dependencies",
-            dependencies: [
-                .product(
-                    name: "WorkoutsPlayerCore",
-                    package: "workouts-player-core-internal-ios",
-                    condition: .when(platforms: [.iOS])
-                )
-            ],
-            path: "Sources"
-        ),
         .binaryTarget(
             name: "WorkoutsPlayer",
             url: "https://github.com/muuvlabs/af-workouts-player-ios/releases/download/\(version)/WorkoutsPlayer_\(version).xcframework.zip",
